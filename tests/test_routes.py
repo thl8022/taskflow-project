ee5bb9a5e5fd738106c42575f7cac8d9e9b337e1
Thiago Henrique <thl8022@gmail.com>
2026-07-06 02:39:35 -0300

    test: ampliacao dos testes do CRUD

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,7 @@
 import pytest
 
 from app import app
+from src.services import create_task, get_all_tasks
 
 
 @pytest.fixture
@@ -24,5 +25,38 @@ def test_dashboard_page(client):
 
 def test_new_task_page(client):
     response = client.get("/new")
+
+    assert response.status_code == 200
+
+def test_create_task(client):
+
+    response = client.post(
+        "/new",
+        data={
+            "title": "Teste Pytest",
+            "description": "Descrição de teste",
+            "priority": "Alta",
+            "status": "Pendente"
+        },
+        follow_redirects=True
+    )
+
+    assert response.status_code == 200
+
+def test_delete_task(client):
+
+    create_task(
+        "Excluir",
+        "Teste",
+        "Baixa",
+        "Pendente"
+    )
+
+    task = get_all_tasks()[0]
+
+    response = client.get(
+        f"/delete/{task['id']}",
+        follow_redirects=True
+    )
 
     assert response.status_code == 200
